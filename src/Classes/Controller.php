938629da7bca233d32a0bfb4dca53ee4c7e029e3
inghamn <inghamn@bloomington.in.gov>
2016-03-31 12:08:55 -0400

    Controllers should now create and return Templates

--- src/Classes/Controller.php
+++ src/Classes/Controller.php
@@ -2,21 +2,12 @@
 /**
  * @copyright 2012-2016 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
- * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
 namespace Blossom\Classes;
 
 abstract class Controller
 {
-	protected $template;
-
 	abstract public function index();
-
-	public function __construct(Template &$template)
-	{
-		$this->template = $template;
-		$this->template->controller = get_class($this);
-	}
 
 	/**
 	 * Returns the full URL for a named route
